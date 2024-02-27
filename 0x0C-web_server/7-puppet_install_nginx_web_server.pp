43ebb7ccbfbc157bdc8488f4e996161ffeba664b
Abdallah616 <yassinabdou59@gmail.com>
2024-02-27 09:59:32 -0800

    solved

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,63 +1,24 @@
-# Install Nginx package
+# Setup New Ubuntu server with nginx
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => present,
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-# Define Nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  hasrestart => true,
-  hasstatus  => true,
-  require   => Package['nginx'],
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-# Configure Nginx site
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-server {
-    listen 80;
-
-    root /var/www/html;
-    index index.html index.htm;
-
-    location / {
-        # Return Hello World! for root path
-        return 200 'Hello World!';
-    }
-
-    location /redirect_me {
-        # Perform a 301 redirect for /redirect_me
-        return 301 http://\$host/;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        root /usr/share/nginx/html;
-        internal;
-    }
-
-    location ~ \.php$ {
-        include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        include fastcgi_params;
-    }
-}
-  ",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-# Create document root
-file { '/var/www/html':
-  ensure => directory,
-}
-
-# Reload Nginx when the configuration changes
-exec { 'nginx-reload':
-  command     => 'service nginx reload',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
